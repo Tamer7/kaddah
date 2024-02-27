e7aa72653c825d45e517b712226854e897aed17f
Ihor nepsha <jennifer39barbourtui@outlook.com>
2024-02-27 09:38:42 +0200

    fix: point 7

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -8,7 +8,7 @@
     </x-slot>
   </x-panel>
 
-  <div class="lg:container py-8 grid lg:grid-cols-3 lg:gap-3 sm:grid-cols-2 gap-4 px-8">
+  <div class="lg:container py-8 grid lg:grid-cols-3 lg:gap-6 sm:grid-cols-2 gap-4 px-8">
     @foreach ($events as $event)
       <x-items.event :event="$event" />
     @endforeach
