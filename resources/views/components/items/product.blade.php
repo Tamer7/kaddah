3c1270ec3618789e28984d618fd0e6138f1a7f0b
Ihor nepsha <jennifer39barbourtui@outlook.com>
2024-02-27 03:27:56 +0200

    fix: point 5

--- resources/views/components/items/product.blade.php
+++ resources/views/components/items/product.blade.php
@@ -1,12 +1,12 @@
 <!-- attributes: product => [img, category.title, url(optional), title, type1, type2] -->
 
 <div class="px-4 py-2 lg:text-left text-center">
-  <img class="mx-auto sm:max-h-[320px] max-h-[200px] min-h-[200px]" src="{{ asset('storage/' . $product->image) }}" alt="">
-  <div class="pt-5">{{ $category['title'] ?? 'Products' }}</div>
   <a href="{{ route('products.product', ['product' => $product->id, 'category_id' => $category['id'] ?? '']) }}" class="underline">
+    <img class="mx-auto sm:max-h-[320px] max-h-[200px] min-h-[200px]" src="{{ asset('storage/' . $product->image) }}" alt="">
+    <div class="pt-5">{{ $category['title'] ?? 'Products' }}</div>
     <div class="overflow-hidden text-ellipsis font-semibold text-lg whitespace-nowrap py-2">{{ $product->name }}</div>
+    <div class="overflow-hidden text-clip whitespace-nowrap">
+      {{ $product->price }} | <span class="font-bold">{{ $product->origin }}</span>
+    </div>
   </a>
-  <div class="overflow-hidden text-clip whitespace-nowrap">
-    {{ $product->price }} | <span class="font-bold">{{ $product->origin }}</span>
-  </div>
 </div>
