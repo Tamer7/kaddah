8dd7bc46a76960d25a6cf982eb8f2ed6591cf4bd
tamerjar <algarmakany.ext@nubedian.de>
2024-03-01 19:50:26 +0200

    Making brand page

--- resources/views/brands/brand.blade.php
+++ resources/views/brands/brand.blade.php
@@ -1,141 +1,48 @@
-@extends('master')
+@php
+$breadcrumbs = ["Brands" => "/brands"];
+@endphp
 
-@section('title', $brand->name)
+<x-layout title="{{ $brand->name ?? '' }}">
+    <x-panel :breadcrumbs="$breadcrumbs">
+        <x-slot name="title">{{ $brand->name ?? 'Products' }}</x-slot>
 
-@section('content')
 
- <!-- Start of Main -->
- <main class="main">
-    <!-- Start of Breadcrumb -->
-    <nav class="breadcrumb-nav">
-        <div class="container">
-            <ul class="breadcrumb bb-no">
-                <li><a href="/">Home</a></li>
-                <li><a href="/brands">Brands</a></li>
-                <li>{{$brand->name}}</li>
-            </ul>
-        </div>
-    </nav>
-    <!-- End of Breadcrumb -->
+    <!-- <div class="lg:container py-8 grid lg:grid-cols-3 lg:gap-3 sm:grid-cols-2">
+    <form action="{{ route('brands.show', $brand->id ?? '') }}" method="GET" class="col-start-1">
+    <label for="sort" class="mr-2">Sort By:</label>
+        <select name="sort" id="sort" onchange="this.form.submit()" class="border border-gray-300 rounded-md px-2 py-1">
+          <option value="">Default sorting</option>
+          <option value="old" {{ request()->sort == 'old' ? 'selected' : '' }}>Sort by Oldest</option>
+          <option value="ASC" {{ request()->sort == 'ASC' ? 'selected' : '' }}>Sort by Code: A to Z</option>
+          <option value="DESC" {{ request()->sort == 'DESC' ? 'selected' : '' }}>Sort by Code: Z to A</option>
+        </select>
+    </form>
 
-    <!-- Start of Page Content -->
-    <div class="page-content mb-10">
-        <div class="shop-default-banner shop-boxed-banner banner d-flex align-items-center mb-6"
-            style="background-image: url({{asset('/images/categories/default.jpg')}}); background-color: var(--primary-color);">
-            <div class="container banner-content">
-                <h3 class="banner-title text-white text-uppercase font-weight-bolder ls-10">{{$brand->name}}</h3>
-            </div>
-        </div>
-        <!-- End of Shop Banner -->
-        <div class="container-fluid">
-            <!-- Start of Shop Content -->
-            <div class="shop-content">
-                <!-- Start of Shop Main Content -->
-                <div class="main-content">
-                    <nav class="toolbox sticky-toolbox sticky-content fix-top">
-                        <div class="toolbox-left">
-                            <div class="toolbox-item toolbox-sort select-box text-dark">
-                                <label>Sort By :</label>
-                                <select name="orderby" class="form-control" onchange="window.location.href=this.value;">
-                                    <option value="{{ route('brands.show', $brand->id) }}" {{ request()->sort ? '' : 'selected' }}>Default sorting</option>
-                                    <option value="{{ route('brands.show', ['brand'=> $brand->id, 'sort' => 'old']) }}" {{ request()->sort == 'old' ? 'selected' : '' }}>Sort by Oldest</option>
-                                    <option value="{{ route('brands.show', ['brand'=> $brand->id, 'sort' => 'ASC']) }}" {{ request()->sort == 'ASC' ? 'selected' : '' }}>Sort by Code: A to Z</option>
-                                    <option value="{{ route('brands.show', ['brand'=> $brand->id, 'sort' => 'DESC']) }}" {{ request()->sort == 'DESC' ? 'selected' : '' }}>Sort by Code: Z to A</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="toolbox-right">
-                            <div class="toolbox-item toolbox-show select-box">
-                                <select name="count" class="form-control" onchange="window.location.href=this.value;">
-                                    <option value="{{ route('brands.show', ['brand'=> $brand->id, 'sort' => request()->sort, 'show' => '12']) }}"  {{ request()->show == '12' ? 'selected' : '' }} data-display="Show: 12">Show 12</option>
-                                    <option value="{{ route('brands.show', ['brand'=> $brand->id, 'sort' => request()->sort, 'show' => '20']) }}"  {{ request()->show == '20' ? 'selected' : '' }} {{ request()->show == '' ? 'selected' : '' }} data-display="Show: 20">Show 20</option>
-                                    <option value="{{ route('brands.show', ['brand'=> $brand->id, 'sort' => request()->sort, 'show' => '40']) }}"  {{ request()->show == '40' ? 'selected' : '' }} data-display="Show: 40">Show 40</option>
-                                    <option value="{{ route('brands.show', ['brand'=> $brand->id, 'sort' => request()->sort, 'show' => '70']) }}" {{ request()->show == '70' ? 'selected' : '' }} data-display="Show: 70">Show 70</option>
-                                </select>
-                            </div>
+    <form action="{{ route('brands.show', $brand->id ?? '') }}" method="GET" class="lg:col-start-3 sm:col-start-2">
+    <label for="show" class="mr-2">Show:</label>
+          <select name="show" id="show" onchange="this.form.submit()" class="border border-gray-300 rounded-md px-2 py-1">
+            <option value="12" {{ request()->show == '12' ? 'selected' : '' }}>Show 12</option>
+            <option value="20" {{ request()->show == '20' || !request()->has('show') ? 'selected' : '' }}>Show 20</option>
+            <option value="40" {{ request()->show == '40' ? 'selected' : '' }}>Show 40</option>
+            <option value="70" {{ request()->show == '70' ? 'selected' : '' }}>Show 70</option>
+          </select>
+    </form>
+    </div> -->
 
-                        </div>
-                    </nav>
-                    <div class="product-wrapper row cols-xl-6 cols-lg-5 cols-md-4 cols-sm-3 cols-2">
-                        @foreach($products as $product)
-                        <?php
-                            $child = $product->categories()->first();
-                            if($child)
-                            {
-                                $parent = $child->parentId;
-                            } else {
-                                dd($product);
-                            }
 
-                            // dd($product);
-                        ?>
-                        <div class="product-wrap">
-                            <div class="product text-center">
-                                <figure class="product-media">
-                                    <a href="{{route('product.show', ['parent' => $parent->slug, 'child' => $child->slug, 'slug' => $product->slug])}}">
-                                        <img src="{{asset('storage/'.$product->image)}}" alt="Product" width="300"
-                                            height="338" />
-                                        @if($product->images)
-                                            @foreach(json_decode($product->images) as $image)
-                                                @if($loop->last)
-                                                <img src="{{asset('storage/'.$image)}}" alt="Product" width="300"
-                                                    height="338" />
-                                                @endif
-                                            @endforeach
-                                        @endif
-                                    </a>
-                                    <div class="product-action-horizontal">
-                                        <a href="javascript:void(0)" onClick="$('.cart-submit-{{$product->id}}').submit();"
-                                            class="btn-product-icon btn-cart-design w-icon-cart loader-alert-{{$product->id}}"
-                                            title="Add to cart"></a>
 
-                                        <form action="javascript:void(0)" method="POST" class="d-none add-to-cart cart-submit-{{$product->id}}" id="{{$product->id}}">
-                                            @csrf
-                                            <input type="hidden" name="id" value="{{$product->id}}">
-                                        </form>
 
-                                        <button class="d-none btn-cart" id="cart-response-{{$product->id}}">
-                                        </button>
-{{--                                        <a href="#" class="btn-product-icon btn-wishlist w-icon-heart"--}}
-{{--                                            title="Wishlist"></a>--}}
-{{--                                        <a href="#" class="btn-product-icon btn-compare w-icon-compare"--}}
-{{--                                            title="Compare"></a>--}}
-                                        <a href="javascript:void(0);" class="show-modal btn-product-icon  w-icon-search"
-                                            title="Quick View" id="{{$product->slug}}"></a>
-                                            <a href="javascript:void(0);" class="d-none btn-quickview" id="quickview-{{$product->id}}"></a>
-                                    </div>
-                                </figure>
-                                <div class="product-details">
-                                    <div class="product-cat">
-                                        <a href="/category/{{$parent->slug}}">{{$child->name}}</a>
-                                    </div>
-                                    <h3 class="product-name">
-                                        <a href="{{route('product.show', ['parent' => $parent->slug, 'child' => $child->slug, 'slug' => $product->slug])}}">{{$product->name}}</a>
-                                    </h3>
-                                    <div class="ratings-container">
-                                        <a href="/brands/{{$brand->slug}}" class="rating-reviews">{{$product->brand->name}}</a>
-                                    </div>
-                                    <div class="product-pa-wrapper">
-                                        <div class="product-price">
-                                            <ins class="new-price">{{$product->code}}</ins>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-                    {{ $products->appends(request()->input())->links() }}
+    @if($products->count() > 0)
+      <div class="lg:container py-8 grid lg:grid-cols-3 lg:gap-3 sm:grid-cols-2">
+        @foreach ($products as $product)
+          <x-items.brand :product="$product" :brand="$brand" />
+        @endforeach
+      </div>
+    @elseif(request()->has('search'))
+      <div class="text-center py-8">
+        <h3 class="wishlist-title text-[20px]">Your search query for <span class="text-primary font-bold underline">{{ request()->query('search') }}</span> showed {{ $products->total() }} {{ $products->total() == 1 ? 'result' : 'results' }}</h3>
+      </div>
+    @endif
 
-                </div>
-                <!-- End of Shop Main Content -->
-
-            </div>
-            <!-- End of Shop Content -->
-        </div>
-    </div>
-    <!-- End of Page Content -->
-</main>
-<!-- End of Main -->
-
-@endsection
+  </x-panel>
+</x-layout>
