2cce1dcabcadbada3361bf68a7d1663c0caace1c
TamerJar <tamer.algarmakany@visma.com>
2024-04-10 21:08:11 +0300

    Final ever change

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -1,46 +1,12 @@
-@extends('master')
-
-@section('content')
-
-<!-- Start of Main -->
-<main class="main">
-    <!-- Start of Page Header -->
-    <div class="page-header" style="display:none;">
-        <div class="container">
-            <h1 class="page-title mb-0">Thank you</h1>
-        </div>
-    </div>
-    <!-- End of Page Header -->
-
-    <!-- Start of Breadcrumb -->
-    <!-- <nav class="breadcrumb-nav mb-10 pb-8">
-        <div class="container">
-            <ul class="breadcrumb">
-                <li><a href="/">Home</a></li>
-                <li>Thank you</li>
-            </ul>
-        </div>
-    </nav> -->
-    <!-- End of Breadcrumb -->
-
-    <!-- Start of Page Content -->
-    <br />
-    <br />
-    <div class="page-content">
-        <div class="container">
-            <section class="introduce mb-10 pb-10">
-                <h2 class="title title-center">
-                    We'll get back to you soon!
-                </h2>
-                <p class=" mx-auto text-center">Thank you for submitting an inquiry! Our sales team will get back to you in 24 hours with an
-                  official quote. For urgent inquiries, please contact our sales support at: +971 4 238 4774
-                </p>
-
-            </section>
-
-        </div>
-    </div>
-</main>
-<!-- End of Main -->
-
-@endsection
+<x-layout title="Quotation Received">
+  <x-panel>
+    <x-slot name="title">
+    We'll get back to you soon!
+    </x-slot>
+    <x-slot name="content" class="mt-20">
+        Thank you for submitting an inquiry! Our sales team will get back to you in 24 hours with an
+        official quote. For urgent inquiries, please contact our sales support at:
+        <a href="tel:+97142384774" class="text-[theme(colors.blue)]"><b>+971 4 238 4774</b></a>.
+    </x-slot>
+  </x-panel>
+</x-layout>
